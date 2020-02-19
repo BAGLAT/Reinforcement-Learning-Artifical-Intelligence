c3ec6238d8f543e8e9d29b546f45dd18214b918d
scrntnstrnglr <bhattasi@tcd.ie>
2020-02-19 10:46:07 +0000

    Added first py file and installed pandas

---
+++ AIDemo/cpp.py
@@ -0,0 +1,10 @@
+import itertools
+import copy
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("Hello World!")
+
+edgelist = pd.read_csv('https://gist.githubusercontent.com/brooksandrew/e570c38bcc72a8d102422f2af836513b/raw/89c76b2563dbc0e88384719a35cba0dfc04cd522/edgelist_sleeping_giant.csv')
+nodelist = pd.read_csv('https://gist.githubusercontent.com/brooksandrew/f989e10af17fb4c85b11409fea47895b/raw/a3a8da0fa5b094f1ca9d82e1642b384889ae16e8/nodelist_sleeping_giant.csv')
